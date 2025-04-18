21dd4be22ffda6251cd782f095a77c525acc4d6b
1aneck <stetina1@gmail.com>
2025-04-18 18:16:30 +0200

    zadaniHW

--- homework_01.py
+++ homework_01.py
@@ -1,1 +1,56 @@
+'''
+2/6 Zapiš pozdrav a odděl jej pomocnou proměnnou
+VITEJTE U NASI APLIKACE DESTINATIO!
+===================================
 
+3/6 Vypiš nabídku cílových destinací a odděl ji pomocnou proměnnou
+VITEJTE U NASI APLIKACE DESTINATIO!
+===================================
+1 - Praha   | 150
+2 - Viden   | 200
+3 - Olomouc | 120
+4 - Svitavy | 120
+5 - Zlin    | 100
+6 - Ostrava | 180
+
+4/6 Dovol uživateli zadat 'destinace', 'cele_jmeno', 'email', 'rok_narozeni' a doplň oddělovač
+<predchozi_ukoly>
+===================================
+CISLO DESTINACE: 1
+JMENO: Matous
+PRIJMENI: Holinka
+EMAIL: matous@matous.cz
+===================================
+
+5/6 Zkus propojit stávající datový typ "mesta" a "destinace"
+<predchozi_ukoly>
+===================================
+CISLO DESTINACE: 2
+===================================
+Viden
+200
+===================================
+
+6/6 Vypiš tyto informace pro objednávajícího uživatele
+ - Kdo si objednal,
+ - kam cestuje a za kolik,
+ - kam mu přijde lístek.
+VITEJTE U NASI APLIKACE DESTINATIO!
+===================================
+1 - Praha   | 150
+2 - Viden   | 200
+3 - Olomouc | 120
+4 - Svitavy | 120
+5 - Zlin    | 100
+6 - Ostrava | 180
+===================================
+CISLO DESTINACE: 2
+JMENO: Matous
+PRIJMENI: Holinka
+EMAIL: matous@matous.cz
+===================================
+DEKUJI, Matous ZA OBJEDNAVKU,
+CIL. DESTINACE: Viden, CENA JIZDNEHO: 200,
+NA TVUJ MAIL matous@matous.cz JSME TI POSLALI LISTEK.
+===================================
+'''
