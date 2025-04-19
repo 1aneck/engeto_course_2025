12883b1150877aa9dd3587171f63470c7905765f
1aneck <stetina1@gmail.com>
2025-04-19 18:56:15 +0200

    done

--- homework_01.py
+++ homework_01.py
@@ -54,3 +54,42 @@ CIL. DESTINACE: Viden, CENA JIZDNEHO: 200,
 NA TVUJ MAIL matous@matous.cz JSME TI POSLALI LISTEK.
 ===================================
 '''
+
+# 2/6
+greetings = "VITEJTE U NASI APLIKACE DESTINATIO\n"
+article_separator = "=" * len(greetings)
+main_header = greetings + article_separator
+print(main_header)
+
+# 3/6
+our_offer = """
+1 - Praha   | 150
+2 - Viden   | 200
+3 - Olomouc | 120
+4 - Svitavy | 120
+5 - Zlin    | 100
+6 - Ostrava | 180
+"""
+print(our_offer)
+print(article_separator)
+
+# 4/6
+full_name = input("Insert your full name: ")
+date_of_birth = input("Insert your date of birth: ")
+email_address = input("Insert your e-mail: ")
+destination_number = input("Insert the destination number: ")
+print(article_separator)
+
+# 5/6
+destination_list = ["Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava"]
+price_list = [150, 200, 120, 120, 100, 180]
+selected_destination = destination_list[int(destination_number) - 1] 
+selected_destination_price = price_list[int(destination_number) - 1]
+#print("Your order:\nDestination - " + selected_destination + "\nPrice: " + str(selected_destination_price) + ",-\n" + article_separator)
+
+# 6/6
+print(f"""Welcome on board, {full_name}!
+Your trip to {selected_destination} for {selected_destination_price},- has been booked.
+We have sent all important information to your email address - {email_address}.
+Team DESTINATIO
+""")
