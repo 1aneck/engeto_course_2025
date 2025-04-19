c93728d1cfd82ba91838ef3fcbc9afbd106490df
1aneck <stetina1@gmail.com>
2025-04-19 22:23:39 +0200

    HW2 zadani

---
+++ homework_02.py
@@ -0,0 +1,13 @@
+"""
+Vytvoř takový podmínkový zápis, který bude reagovat na nesprávně zadaná hesla (viz. příklad níže):
+heslo_0 = ""            # FAIL -> "Vynechal jsi pole s heslem!"
+heslo_1 = "1panpes738"  # FAIL -> "Heslo nesmí začínat číselným znakem"
+heslo_2 = "panpessss"   # FAIL -> "Heslo musí obsahovat jak číselné znaky, tak písmena"
+heslo_3 = "123456"      # FAIL -> "Heslo nesmí začínat číselným znakem"
+heslo_4 = "aa1234"      # FAIL -> "Heslo musí být alespoň 8 znaků dlouhé"
+heslo_5 = "p@npes7778"  # FAIL -> "Heslo nesmí obsahovat '@'"
+heslo_6 = "panpes7778"  # PASS -> "Heslo je v pořádku"
+
+heslo = heslo_6
+"""
+
